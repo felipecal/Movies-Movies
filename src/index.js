91af3f9a9e65bc414bf10462b58aba9b2f9937dc
Felipe <fcalldas@gmail.com>
2022-07-11 20:51:06 -0300

    ✨ new features

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import {ApolloServer} from 'apollo-server';
+import database from './db/models';
+
+import resolvers from './graphql/resolvers';
+import typeDefs from './graphql/typeDefs';
+
+const server = new ApolloServer ({
+    typeDefs, resolvers, context: ({req}) => ({
+        database, 
+        req
+    })
+});
+
+const app = process.env.PORT || 4004;
+
+server.listen(app).then(() => console.log(`🔥 Server ready at ${app} 🔥`));
