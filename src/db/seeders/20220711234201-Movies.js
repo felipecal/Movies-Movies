e869ae5c5c4b0705ca8bbdfb0a1fbb3887ce6ac3
felipecal <fcalldas@gmail.com>
2022-07-12 23:00:36 -0300

    🌱 update seed files

--- src/db/seeders/20220711234201-Movies.js
+++ src/db/seeders/20220711234201-Movies.js
@@ -1,24 +1,25 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Movies',
+      [{
+        name: 'Minions 2',
+        year: 2022
+      },
+      {
+        name: 'Miranha Back to home',
+        year: 2022
+      },
+      {
+        name: 'Eternos',
+        year: 2021
+      }], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete('Movies', null, {});
+
   }
 };
